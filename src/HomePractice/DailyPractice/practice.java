dfd02d3a02474fab19982430951d831b31ceb825
Sampa Nayak <sampanayak987@gmail.com>
2026-04-15 18:43:07 +0530

    msg

--- src/HomePractice/DailyPractice/practice.java
+++ src/HomePractice/DailyPractice/practice.java
@@ -2,9 +2,7 @@ package HomePractice.DailyPractice;
 
 public class practice {
     static void main() {
-        int[] a = new int[3];
-        int[] b = a;
-        b[0] = 100;
-        System.out.println(a[0]);
+         String str = "appla";
+        System.out.println(str.replace('a','A'));
     }
 }
