42d29463e9e2f3aea8fc9b660e1d64071ca26c01
Sampa Nayak <sampanayak987@gmail.com>
2026-04-24 18:20:23 +0530

    msg

---
+++ src/HomePractice/GeeksForGeeks/ProblemOfTheDay/BuildingWithSunlight.java
@@ -0,0 +1,28 @@
+package HomePractice.GeeksForGeeks.ProblemOfTheDay;
+
+import java.util.Stack;
+
+public class BuildingWithSunlight {
+    static void main(String[] args) {
+        int []arr = {6, 2, 8, 4, 11, 13};
+        System.out.println(visibleBuildings(arr));
+    }
+    public static int visibleBuildings(int arr[]) {
+        // code here
+        Stack<Integer> st = new Stack<>();
+        int c = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            while(!st.isEmpty() && st.peek() <= arr[i])
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                c++;
+            }
+            st.push(arr[i]);
+        }
+        return c;
+    }
+}
