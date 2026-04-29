d5e4fb4e1de3eea9d705228c126d71b399a2d847
Sampa Nayak <sampanayak987@gmail.com>
2026-04-29 08:42:00 +0530

    msg

---
+++ src/HomePractice/GeeksForGeeks/ProblemOfTheDay/MinimumSwapOfGroup1S.java
@@ -0,0 +1,58 @@
+package HomePractice.GeeksForGeeks.ProblemOfTheDay;
+/*
+You are given a binary array arr[] consisting only of 0s and 1s. Determine the minimum number of swaps required to group all the 1s together in a contiguous subarray.
+
+A swap operation allows you to choose any two indices i and j and exchange their values (i.e., swap(arr[i], arr[j])).
+
+If the array contains no 1s, return -1.
+
+Examples:
+
+Input: arr[] = [1, 0, 1, 0, 1]
+Output: 1
+Explanation: Only 1 swap is required to group all 1's together. Swapping index 1 and 4 will give arr[] = [1, 1, 1, 0, 0].
+Input: arr[] = [1, 0, 1, 0, 1, 1]
+Output: 1
+Explanation: Only 1 swap is required to group all 1's together. Swapping index 0 and 3 will give arr[] = [0, 0, 1, 1, 1, 1].
+Input: arr[] = [0, 0, 0]
+Output: -1
+Explanation: No 1s are present in the array, so return -1.
+ */
+public class MinimumSwapOfGroup1S {
+    static void main() {
+        int []arr = {1, 0, 1, 0, 1, 1};
+        System.out.println(minSwaps(arr));
+    }
+    public static int minSwaps(int[] arr) {
+        // code here
+        int one = 0;
+        for(int ele : arr)
+        {
+            if(ele == 1)
+            {
+                one++;
+            }
+        }
+        if(one == 0)
+        {
+            return -1;
+        }
+        int currOne = 0;
+        int maxOne = 0;
+        for(int i = 0; i < one; i++)
+        {
+            if(arr[i] == 1)
+            {
+                currOne++;
+            }
+        }
+        maxOne = currOne;
+        for(int i = one; i < arr.length; i++)
+        {
+            if(arr[i] == 1) currOne++;
+            if(arr[i-one] == 1) currOne--;
+            maxOne = Math.max(maxOne, currOne);
+        }
+        return one - maxOne;
+    }
+}
