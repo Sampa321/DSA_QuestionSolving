57e5d5c5c0c9d04bac659d437d23a06f80c7e50a
Sampa Nayak <sampanayak987@gmail.com>
2026-05-02 22:39:02 +0530

    msg

---
+++ src/HomePractice/GeeksForGeeks/ProblemOfTheDay/Basic/PositionOfTheSetBit.java
@@ -0,0 +1,35 @@
+package HomePractice.GeeksForGeeks.ProblemOfTheDay.Basic;
+/*
+Given an integer n, determine position of the only set bit (1) in its binary representation. The position is counted starting from 1 at the least significant bit (LSB).
+
+If n contains exactly one set bit, return its position.
+If n contains no set bits or more than one set bit, return -1.
+Examples:
+
+Input: n = 2
+Output: 2
+Explanation: 2 is represented as "10" in binary. It has only one set bit, which is at position 2.
+Input: n = 5
+Output: -1
+Explanation: 5 is represented as "101" in binary. It has two set bits; therefore, the output is -1.
+ */
+public class PositionOfTheSetBit {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(findPosition(n));
+    }
+    public static int findPosition(int n) {
+        // check if n has exactly one set bit
+        if (n == 0 || (n & (n - 1)) != 0) {
+            return -1;
+        }
+
+        int pos = 1;
+        while ((n & 1) == 0) {
+            n >>= 1;
+            pos++;
+        }
+        return pos;
+    }
+}
+
