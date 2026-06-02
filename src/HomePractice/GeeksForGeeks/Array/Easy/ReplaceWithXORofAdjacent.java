f448752dbcb45784cb48559fd0cb1ae29c6df38d
Sampa Nayak <sampanayak987@gmail.com>
2026-06-02 18:01:38 +0530

    msg

---
+++ src/HomePractice/GeeksForGeeks/Array/Easy/ReplaceWithXORofAdjacent.java
@@ -0,0 +1,54 @@
+package HomePractice.GeeksForGeeks.Array.Easy;
+
+/*
+Given an array arr[] of n integers, modify the array in-place such that each element is replaced with the XOR of its adjacent elements.
+
+For the first element, update arr[0] = arr[0] ^ arr[1].
+For the last element, update arr[n-1] = arr[n-2] ^ arr[n-1].
+For all other elements, update arr[i] = arr[i-1] ^ arr[i+1].
+Note: Here, a ^ b represents the XOR operation between a and b.
+
+Examples :
+
+Input : arr[] = [2, 1, 4, 7]
+Output : [3, 6, 6, 3]
+Explanation:
+At index 0, arr[0] ^ arr[1] = 2 ^ 1 = 3
+At index 1, arr[0] ^ arr[2] = 2 ^ 4 = 6
+At index 2, arr[1] ^ arr[3] = 1 ^ 7 = 6
+At index 3, arr[2] ^ arr[3] = 4 ^ 7 = 3
+Thus, the updated array becomes [3, 6, 6, 3].
+Input : arr[] = [5, 9, 2, 6, 7]
+Output: [12, 7, 15, 5, 1]
+Explanation:
+At index 0, arr[0] ^ arr[1] = 5 ^ 9 = 12
+At index 1, arr[0] ^ arr[2] = 5 ^ 2 = 7
+At index 2, arr[1] ^ arr[3] = 9 ^ 6 = 15
+At index 3, arr[2] ^ arr[4] = 2 ^ 7 = 5
+At index 4, arr[3] ^ arr[4] = 6 ^ 7 = 1
+Thus, the updated array becomes [12, 7, 15, 5, 1].
+ */
+public class ReplaceWithXORofAdjacent {
+    public static void main(String[] args) {
+       int []arr = {5, 9, 2, 6, 7};
+        System.out.println(replaceElements(arr));
+    }
+    public static boolean replaceElements(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        int first = arr[0] ^ arr[1];
+        int last = arr[n - 2] ^ arr[n - 1];
+        int prev = arr[0];
+
+        for (int i = 1; i < n - 1; i++) {
+            int curr = arr[i];
+            arr[i] = prev ^ arr[i + 1];
+            prev = curr;
+        }
+
+        arr[0] = first;
+        arr[n - 1] = last;
+        return false;
+    }
+}
