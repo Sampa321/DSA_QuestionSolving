9fc658d8fad5b9300a530ee74729b1a6b4702192
Sampa Nayak <sampanayak987@gmail.com>
2026-07-13 14:58:32 +0530

    Problem Solving

---
+++ src/HomePractice/Leetcode/DailySolvingProblem/SequentialDigits.java
@@ -0,0 +1,36 @@
+package HomePractice.Leetcode.DailySolvingProblem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 1291.
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+Example 1:
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ */
+public class SequentialDigits {
+   public static void main(String[] args) {
+        int low = 1000, high = 13000;
+       System.out.println(sequentialDigits(low, high));
+    }
+    public static List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new ArrayList<>();
+        for(int i = 1; i <= 9; i++)
+        {
+            int num = i;
+            for(int j = i+1; j <= 9; j++)
+            {
+                num = num*10+j;
+                if(num >= low && num <= high) result.add(num);
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+}
