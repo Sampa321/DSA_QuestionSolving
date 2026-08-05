98af8ee50660e1d5e0ba4f008e1b7a01e74fa3bf
Sampa Nayak <sampanayak987@gmail.com>
2026-08-05 10:04:24 +0530

    Problem Solving

--- src/HomePractice/DailyPractice/Practice/Queue.java
+++ src/HomePractice/DailyPractice/Practice/Queue.java
@@ -1,86 +1,25 @@
 package HomePractice.DailyPractice.Practice;
 
 public class Queue {
-     int []arr;
-     int capacity;
-     int size;
-     int front;
-     Queue(int c){
-         this.capacity = c;
-         this.arr = new int[capacity];
-         this.size = 0;
-         this.front = -1;
-     }
-
-     public boolean isEmpty(){
-         return this.size == 0;
-     }
-
-    public boolean isFull(){
-        return this.size == this.capacity;
-    }
-
-    public int getFront(){
-         if (this.isEmpty())
+     public static void main(String []args)
+     {
+         int []gas = {4, 5, 7, 4};
+         int []cost = {6, 6, 3, 5};
+         for(int i = 0; i < gas.length; i++)
          {
-             System.out.println("Queue is empty!!");
-             return Integer.MAX_VALUE;
+             int end = i;
+             int curr_petrol = 0;
+             while (true)
+             {
+                 curr_petrol += (gas[end] - cost[end]);
+                 if(curr_petrol < 0) break;
+                 if(i == end)
+                 {
+                     System.out.println(i);
+                     return;
+                 }
+             }
          }
-         return arr[this.front];
-    }
-
-    public int getRear(){
-        if (this.isEmpty())
-        {
-            System.out.println("Queue is empty!");
-            return Integer.MAX_VALUE;
-        }
-        int rear = (this.front + this.size-1)%this.capacity;
-        return arr[rear];
-    }
-
-    public void enqueue(int val)
-    {
-        if(this.isFull())
-        {
-            System.out.println("Queue is full");
-            return;
-        }
-        if (this.isEmpty())
-        {
-            this.front = 0;
-//            this.arr[this.front] = val;
-//            this.size++;
-//            return;
-        }
-        int rear = (this.front + this.size-1)%this.capacity;
-        this.arr[rear] = val;
-        this.size++;
-    }
-
-
-    public int dequeue(){
-        if(this.isEmpty())
-        {
-            System.out.println("Queue is Empty!!");
-            return Integer.MAX_VALUE;
-        }
-        int re = this.arr[this.front];
-        this.front = (this.front+1) % this.capacity;
-        this.size--;
-        if (this.size == 0)
-        {
-            this.front = -1;
-        }
-        return re;
-    }
-
-   public static void main(String[] args)
-   {
-        Queue qu = new Queue(3);
-        qu.enqueue(10);
-        System.out.println(qu.isEmpty());
-        System.out.println(qu.isFull());
-       System.out.println(qu.dequeue());
-   }
+         System.out.println(-1);
+     }
 }
---
+++ src/Java/PatternPrinting/Example8.java
@@ -0,0 +1,21 @@
+package Java.PatternPrinting;
+
+public class Example8 {
+    public static void main(String[] args) {
+        int n = 4;
+//        for (int i = n; i >= 1; i--)
+//        {
+//            for(int j = 0; j <= n-i; j++)
+//            {
+//                System.out.print("  ");
+//            }
+//            for(int j = 0; j < 2*i+1; j++)
+//            {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+        //}
+    }
+
+
+}
